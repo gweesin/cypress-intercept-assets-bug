51d0d8024c86398dc6417be19979e85735bdc269
GweesinChan <gweesin2000@gmail.com>
2024-06-27 21:46:15 +0800

    chore: init

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,20 @@
 /// <reference types="cypress" />
+
+beforeEach(() => {
+  cy.intercept('**/asset/data.json', { fixture: 'example.json' }).as('asset');
+
+  cy.intercept('**/assets/data.json', { fixture: 'example.json' }).as('assets');
+});
+
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+  it('asset work', () => {
+    fetch('/__cypress/asset/data.json');
+    cy.wait('@asset')
+  })
+
+  // don't work when path includes __cypress/assets
+  it('assets work', () => {
+    fetch('/__cypress/assets/data.json');
+    cy.wait('@assets')
   })
 })
